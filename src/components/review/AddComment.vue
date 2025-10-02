b9d87e9041a771a011f59458cb4036b4a3607ddd
Jeffrey-Meesters <jjeffreymeesters@gmail.com>
2025-10-02 13:39:14 +0200

    Add auto focus when opening a comment section for a question

--- src/components/review/AddComment.vue
+++ src/components/review/AddComment.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
-import type { Question } from "~/types/review";
-import { reviewPlaceholders } from "@/constants/placeholders";
-const question = defineModel<Question>({ required: true });
+  import type { Question } from "~/types/review";
+
+  const question = defineModel<Question>({ required: true });
+  const commentInput = ref<any>(null);
+
+  onMounted(() => {
+    nextTick(() => {
+      if (commentInput.value?.$el) {
+        commentInput.value.$el.focus();
+      }
+    });
+  });
 </script>
 <template>
-  <Textarea
-    v-model="question.comment"
-    placeholder="Any additional comments you want to add?"
-    class="w-full mt-4"
-    rows="5"
-    cols="30"
-  />
+  <Textarea v-model="question.comment" ref="commentInput" placeholder="Any additional comments you want to add?"
+    class="w-full mt-4" rows="5" cols="30" />
 </template>
